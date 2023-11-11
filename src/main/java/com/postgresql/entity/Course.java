91792aa8003d702427103eb45e822703357d2a73
skwasim777 <wasimsk6061@gmail.com>
2023-11-11 18:13:04 +0530

    repository class added to from crud operation

--- src/main/java/com/postgresql/entity/Course.java
+++ src/main/java/com/postgresql/entity/Course.java
@@ -1,7 +1,14 @@
 package com.postgresql.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="COURSE_DTLS")
 public class Course {
-		private Integer id;
-		private String name;
-		private Double price;
+	private Integer id;
+	private String name;
+	private Double price;
 }
