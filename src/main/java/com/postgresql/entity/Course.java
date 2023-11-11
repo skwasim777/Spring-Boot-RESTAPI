1845038d90e68d8d3d1a851712a70cf7c2523e42
skwasim777 <wasimsk6061@gmail.com>
2023-11-11 23:45:55 +0530

    crud operation by using spring boot has been done

--- src/main/java/com/postgresql/entity/Course.java
+++ src/main/java/com/postgresql/entity/Course.java
@@ -1,13 +1,19 @@
 package com.postgresql.entity;
 
+import jakarta.annotation.Generated;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
 @Entity
-@Table(name="COURSE_DTLS")
+@Table(name = "COURSE_DTLS")
 public class Course {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
 	private Double price;
